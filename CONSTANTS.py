90f45fba689d4f3c01e128b9bfcad418c0271a77
sucheen <sucheen.sundaram@gmail.com>
2025-05-07 15:38:05 -0400

    updates to pipeline

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -2,10 +2,12 @@ EMPTY_STRING = '-'
 CMC_KEY = "98464488-2db9-4ddc-9b98-6b48f8b623dc"
 LIMIT = 365
 TRAINING_COLUMNS = 'training_columns.txt'
-COIN = 'BTC'
+COIN = 'ETH'
 TRAIN_PCT = 0.8
 MODEL = "mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis"
 PATH = f'newspapers/{COIN}_newspapers.csv'
+SLEEP = 3
+SERPAPI_KEY = "d8b42ca90077ec59802d53ad3d55e7e05d38ebac54426fbfa8913b8f40f68e24"
 
 # Early Stopping Constants
 LOSS_MULTIPLIER = 1
