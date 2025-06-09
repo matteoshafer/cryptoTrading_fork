927a91e87548fa6391dc4b004b383e4eb475a7ec
sucheen <sucheen.sundaram@gmail.com>
2025-06-09 11:45:22 -0700

    updates and daily data

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -1,8 +1,9 @@
+COIN = 'BTC'
+
 EMPTY_STRING = '-'
 CMC_KEY = "98464488-2db9-4ddc-9b98-6b48f8b623dc"
 LIMIT = 365
 TRAINING_COLUMNS = 'training_columns.txt'
-COIN = 'ETH'
 TRAIN_PCT = 0.8
 MODEL = "mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis"
 PATH = f'newspapers/{COIN}_newspapers.csv'
